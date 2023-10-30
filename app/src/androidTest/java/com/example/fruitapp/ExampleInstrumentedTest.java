1815af152180028deaacfe8945d39d9a3abd02d3
CHRISTOPHER GAVRA RESWARA <christopher.reswara@binus.edu>
2023-10-30 21:33:30 +0700

    Coba Unit Testing

--- app/src/androidTest/java/com/example/fruitapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fruitapp/ExampleInstrumentedTest.java
@@ -10,6 +10,13 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -22,5 +29,28 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.fruitapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void useVolleyInGoolge(){
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        RequestQueue queue = Volley.newRequestQueue(appContext);
+
+        String url = "https://www.google.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response.substring(0, 50));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error Woi!!!");
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
