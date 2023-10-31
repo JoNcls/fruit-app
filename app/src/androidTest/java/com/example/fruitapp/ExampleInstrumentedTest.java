b393144f00dec960364002c4ae63ec15095a6b98
CHRISTOPHER GAVRA RESWARA <christopher.reswara@binus.edu>
2023-10-31 10:49:37 +0700

    Success Request API in AndroidTest

--- app/src/androidTest/java/com/example/fruitapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fruitapp/ExampleInstrumentedTest.java
@@ -5,17 +5,23 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -32,25 +38,43 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
-    public void useVolleyInGoolge(){
+    public void useVolleyInGoogle(){
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         RequestQueue queue = Volley.newRequestQueue(appContext);
 
-        String url = "https://www.google.com";
+        System.out.println("HELLO WORLD");
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        String url = "";
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        System.out.println(response.substring(0, 50));
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                System.out.println("WKJELEK");
                 System.out.println("Error Woi!!!");
+                System.out.println(error.toString());
             }
-        });
+            }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("ApplicationKey", "");
+                return params;
+            }
+        };
 
         queue.add(stringRequest);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
